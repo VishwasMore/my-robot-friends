75bc07ff497feb8a0bbb67c52e85e8ceb7cb3c5a
Vishwas <vishwasmore2019@gmail.com>
2020-09-08 19:25:44 +0400

    Version 3 : Adding Redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,37 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 
-class App extends Component {
-	constructor(){
-		super()
-		this.state = {
-			robots:[],
-			searchField: ''
-		}
+import { setSearchField, requestRobots } from '../actions'
+
+const mapStateToProps = state => {
+	return {
+		searchField : state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
 	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
+	}
+}
+
+class App extends Component {
 
 	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(response =>  response.json())
-			.then(users => this.setState({ robots: users }))
+		this.props.onRequestRobots();
 	}
 
-	onsearchchange = (event) => {
-		this.setState({ searchField: event.target.value })
-	}
+	
 
 	render() {
-		const filteredRobots = this.state.robots.filter(robots => {
-			return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+		const { searchField, onSearchChange, robots } = this.props;
+		const filteredRobots = robots.filter(robots => {
+			return robots.name.toLowerCase().includes(searchField.toLowerCase());
 		})
 		return (
 			<div className='tc'> 
 				<h1 className='f1'> My Robot Friends</h1>
-				<SearchBox searchChange={this.onsearchchange}/>
+				<SearchBox searchChange={onSearchChange}/>
 				<Scroll>
 					<ErrorBoundary>
 						<CardList robots={filteredRobots}/>
@@ -44,4 +53,4 @@ class App extends Component {
 	
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
